1a6d9cee596247ac97881ddf2166f95950051b89
susijade <susijade7@gmail.com>
2026-03-31 10:33:11 +0800

    smart decision-making assistant app

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -33,39 +33,4 @@ class ResultScreen extends StatelessWidget{
       ),
     );
   }
-}import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
-import '../services/decision_service.dart';
-
-class ResultScreen extends StatelessWidget{
-  const ResultScreen ({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final result = Provider.of<DecisionService>(context).currentResult;
-    return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Analyze Results'),
-            bottom: const TabBar(
-                tabs: [
-                  Tab(text: 'Pros/Cons'),
-                  Tab(text: 'Comparison'),
-                  Tab(text: 'SWOT'),
-                ],
-          ),
-        ),
-
-        body: TabBarView(
-            children: [
-              Markdown(data: result!.prosAndCons),
-              Markdown(data: result.comparisonTable),
-              Markdown(data: result.swotAnalysis),
-        ],
-        ),
-      ),
-    );
-  }
 }
